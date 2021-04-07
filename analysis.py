bc2fc6c9fbb7c6d1f5e1365c24fccff6ea5f6db2
unknown <ante.dujic0506@gmail.com>
2021-04-07 11:00:28 +0100

    Testing different data analysis

--- analysis.py
+++ analysis.py
@@ -6,11 +6,41 @@ import pandas as pd
 data = pd.read_csv('irisData.csv')
 
 # print(data.to_string()) 
-print (data.describe())
+
+# a = (data.describe(include = ["object"]))
+# print (a)
+#print (data.describe(include = "all"))          # summary
+
+# print (data.sample (10))        # ten random
+
+# print (data.columns)              # columns
+
+# print (data.shape)               # rows and columns
+
+# print (a.loc [["count", "unique", "freq"]])
+
+
+# species = data.groupby ("species").describe().transpose()
+# print (species)
+
+# count = data.count (axis = "columns")
+
+#print (count)
+"""
+x = data.describe()
+
+f = open("myfile.txt", "w")
+
+f = open("myfile.txt", "a")
+f.write(str (x))
+f.close()
+"""
 
 
 
 """
 REFERENCES:
-- https://www.w3schools.com/python/pandas/pandas_csv.asp
+- read CSV: https://www.w3schools.com/python/pandas/pandas_csv.asp
+- dataset download: https://gist.github.com/curran/a08a1080b88344b0c8a7
+- describe() - https://www.tutorialspoint.com/python_pandas/python_pandas_descriptive_statistics.htm
 """
