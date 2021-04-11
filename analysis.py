8031f744b9b63ad0619db2edd945dfe8343676ef
unknown <ante.dujic0506@gmail.com>
2021-04-11 16:30:39 +0100

    Dataset layout analysis

--- analysis.py
+++ analysis.py
@@ -6,3 +6,12 @@ import pandas as pd
 
 data = pd.read_csv('irisData.csv')
 
+def dataLayout():
+    print ("This data set consists of {} samples, grouped by {} different variables:".format ((data.shape[0]), (data.shape[1])))
+    
+    for col in data.columns:
+        print (col)
+
+    print ("The general layout of the data: \n{}\n*First 10 rows \n{}\n*Random 10 rows" .format ((data.head(10)), data.sample(10)))
+
+dataLayout()
