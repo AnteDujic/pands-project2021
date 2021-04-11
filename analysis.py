f832ba429b7ab26995c383b6c5f01422bb11ff89
unknown <ante.dujic0506@gmail.com>
2021-04-11 14:01:03 +0100

    Separating two programs - test and final

--- analysis.py
+++ analysis.py
@@ -1,46 +1,8 @@
-# Program that analyses iris data set
+# MODULE: Programming and scripting
+# PROJECT: Program that analyses the Iris Data set
 # Author: Ante Dujic
 
 import pandas as pd
 
 data = pd.read_csv('irisData.csv')
 
-# print(data.to_string()) 
-
-# a = (data.describe(include = ["object"]))
-# print (a)
-#print (data.describe(include = "all"))          # summary
-
-# print (data.sample (10))        # ten random
-
-# print (data.columns)              # columns
-
-# print (data.shape)               # rows and columns
-
-# print (a.loc [["count", "unique", "freq"]])
-
-
-# species = data.groupby ("species").describe().transpose()
-# print (species)
-
-# count = data.count (axis = "columns")
-
-#print (count)
-"""
-x = data.describe()
-
-f = open("myfile.txt", "w")
-
-f = open("myfile.txt", "a")
-f.write(str (x))
-f.close()
-"""
-
-
-
-"""
-REFERENCES:
-- read CSV: https://www.w3schools.com/python/pandas/pandas_csv.asp
-- dataset download: https://gist.github.com/curran/a08a1080b88344b0c8a7
-- describe() - https://www.tutorialspoint.com/python_pandas/python_pandas_descriptive_statistics.htm
-"""
