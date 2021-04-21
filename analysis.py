c24a84902726d4713dff5741f4cc4b4612f2d778
unknown <ante.dujic0506@gmail.com>
2021-04-21 10:55:27 +0100

    Customization - dataset variabe names, histogram

--- analysis.py
+++ analysis.py
@@ -10,6 +10,7 @@ import seaborn as sns
 
 # Read the dataset
 data = pd.read_csv('irisData.csv')
+data.columns = ["Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Iris Species"]
 
 # Layout of the data (to be reviewed - merge with other text file)
 def dataLayout():
@@ -24,7 +25,7 @@ with open ("layout.txt", "wt") as f:
     f.write (str (layout))
 
 # Summary data
-speciesNames = data.groupby ("species").size().to_string()
+speciesNames = data.groupby ("Iris Species").size().to_string()
 speciesDetail = (data.describe(include = ["object"])).loc [["count", "unique", "freq"]]
 dataSummary = data.describe ()
 dataCorrelation = data.corr()
@@ -35,20 +36,16 @@ with open ("analysis.txt", "wt") as f:
     f.write ("Data Summary:\n" + str (dataSummary) + "\n\n")
     f.write ("Data correlation:\n" + str (dataCorrelation))
 
-# Histogram (to be optimized)
-
-sepalLength = data ["sepal_length"]
-sepalWidth = data ["sepal_width"]
-petalLength = data["petal_length"]
-petalWidth = data ["petal_width"]
-species = data ["species"]
-
-
-# seaborn (better solution?)
+# Histogram (to be customized)
 fig, axs = plt.subplots(2, 2)
 figsize = (20,20)
-sns.histplot (data, x = "sepal_length", hue = "species", ax = axs[0,0], multiple = "stack")
-sns.histplot (data, x = "sepal_width", hue = "species", ax = axs[0,1], multiple = "stack")
-sns.histplot (data, x = "petal_length", hue = "species", ax = axs[1,0], multiple = "stack")
-sns.histplot (data, x = "petal_width", hue = "species", ax = axs[1,1], multiple = "stack")
+fig.suptitle ("IRIS FEATURE DISTRIBIUTION - by species", size = "xx-large")
+sns.histplot (data, x = "Sepal Length (cm)", hue = "Iris Species", ax = axs[0,0], multiple = "stack")
+axs[0,0].set_title ("SEPAL LENGTH")
+sns.histplot (data, x = "Sepal Width (cm)", hue = "Iris Species", ax = axs[0,1], multiple = "stack")
+axs[0,1].set_title ("SEPAL WIDTH")
+sns.histplot (data, x = "Petal Length (cm)", hue = "Iris Species", ax = axs[1,0], multiple = "stack")
+axs[1,0].set_title ("PETAL LENGTH")
+sns.histplot (data, x = "Petal Width (cm)", hue = "Iris Species", ax = axs[1,1], multiple = "stack")
+axs[1,1].set_title ("PETAL WIDTH")
 plt.show()
